7efe2912baff592c544ca67db39dce2efab1aa10
Tobin Waters <tkwaterss@gmail.com>
2023-02-09 08:40:12 -0700

    instructor solution added

--- challenge-print-digits/printDigits.js
+++ challenge-print-digits/printDigits.js
@@ -59,5 +59,12 @@ reversedInt(test5);
 
 //Solution 3 - Instructors Solution
 const printDigits = num => {
-    
+    while(num){
+        let remainder = num % 10;
+        console.log(remainder);
+        num -= remainder;
+        num /= 10;
+    }
 }
+
+printDigits(12345);
